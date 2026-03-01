3e0586891b10060920576dc0e34fd5e97c95388a
ayushisingh30 <singhayushi3011@gmail.com>
2026-03-01 23:40:37 +0530

    added new programs

---
+++ if_else/else.if/greater.java
@@ -0,0 +1,19 @@
+public class greater {
+
+    public static void main(String[] args) {
+
+        int x = 5;
+        int y = 10;
+
+        if (x == y) {
+            System.out.println("equal");
+        }
+        else if (x > y) {
+            System.out.println("greater");
+        }
+        else {
+            System.out.println("smaller");
+        }
+
+    }
+}
