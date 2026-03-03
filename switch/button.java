fdc59901e3af70741bd6c9a3280eed0759655644
ayushisingh30 <singhayushi3011@gmail.com>
2026-03-03 23:51:08 +0530

    Added new programs

---
+++ switch/button.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class button {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int button = sc.nextInt();
+        switch(button){
+            case 1:System.out.println("hello");
+            break;
+            case 2: System.out.println("konichiwa");
+            break;
+            case 3 : System.out.println("namaste");
+            break;
+            default : System.out.println("invalid button");
+        }
+    }
+}
