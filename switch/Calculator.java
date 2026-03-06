83d2f301529c3440ab8795d55433431d60bbecc9
ayushisingh30 <singhayushi3011@gmail.com>
2026-03-06 23:14:45 +0530

    calculator

---
+++ switch/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 5;
+        char op = '+';
+
+        switch(op) {
+            case '+':
+                System.out.println(a + b);
+                break;
+
+            case '-':
+                System.out.println(a - b);
+                break;
+
+            case '*':
+                System.out.println(a * b);
+                break;
+
+            case '/':
+                System.out.println(a / b);
+                break;
+
+            default:
+                System.out.println("Invalid operator");
+        }
+    }
+}
