da3cf0a62aea2329735c633eedb768789f0d5edb
ayushisingh30 <singhayushi3011@gmail.com>
2026-03-14 00:09:49 +0530

    new

---
+++ OOPS/INHERITANCE/multilevel_inheritance/example.java
@@ -0,0 +1,37 @@
+class A {
+
+    void methodA() {
+        System.out.println("Method of A class");
+    }
+
+}
+
+class B extends A {
+
+    void methodB() {
+        System.out.println("Method of B class");
+    }
+
+}
+
+class C extends B {
+
+    void methodC() {
+        System.out.println("Method of C class");
+    }
+
+}
+
+public class example {
+
+    public static void main(String[] args) {
+
+        C obj = new C();
+
+        obj.methodC();
+        obj.methodB();
+        obj.methodA();
+
+    }
+
+}
