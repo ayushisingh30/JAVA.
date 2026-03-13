583f3e2ab5706b56c071f8e0b4f6fefc6250e024
ayushisingh30 <singhayushi3011@gmail.com>
2026-03-13 11:42:12 +0530

    new

--- OOPS/constructors/example3.java
+++ OOPS/constructors/example3.java
@@ -1,5 +1,16 @@
-package OOPS.constructors;
 
 public class example3 {
+    int modelYear;
+    String modelName;
+
+    public example3(int year,String name){
+        modelYear = year ;
+        modelName = name;
+    }
+
+    public static void main(String[] args) {
+        example3 myCar = new example3(1969,"mustang");
+        System.out.println(myCar.modelYear+" "+myCar.modelName);
+    }
     
 }
