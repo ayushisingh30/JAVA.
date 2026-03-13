7b21c95e4145bc5179e2cb4427f2590cedd59ce4
ayushisingh30 <singhayushi3011@gmail.com>
2026-03-13 11:35:50 +0530

    new constructor

---
+++ OOPS/constructors/example.java
@@ -0,0 +1,15 @@
+
+public class example {
+    int x;
+
+    public example () {
+        x = 5;
+
+    }
+
+    public static void main(String[] args) {
+        example myObj = new example();
+        System.out.println(myObj.x);
+    }
+    
+}
---
+++ OOPS/constructors/example2.java
@@ -0,0 +1,14 @@
+
+public class example2 {
+    int x;
+     
+    public example2(int y ){
+        x= y ;
+
+    }
+     
+    public static void main(String[] args) {
+        example2 myObj = new example2(5);
+        System.out.println(myObj.x);
+    }
+}
